d27e32f725b81717ce4bd930539608485d97261f
Vander Lima <vnd.vander@gmail.com>
2022-11-16 00:31:26 -0300

    fix/remove comments

--- src/routes.ts
+++ src/routes.ts
@@ -2,35 +2,38 @@ import { Router } from 'express';
 
 export const router = Router();
 
-// List categories
 router.get('/categories', (req, res) => {
   res.send('ok');
 });
-// Create category
+
 router.post('/categories', (req, res) => {
   res.send('ok');
 });
-// List products
+
 router.get('/products', (req, res) => {
   res.send('ok');
 });
-// Create product
+
 router.post('/products', (req, res) => {
   res.send('ok');
 });
-// Get products by category
+
 router.get('/categories/:id/products', (req, res) => {
   res.send('ok');
 });
-// List orders
+
 router.get('/orders', (req, res) => {
   res.send('ok');
 });
-// Create order
+
 router.post('/orders', (req, res) => {
   res.send('ok');
 });
-// Change order status
-router.patch('/orders', (req, res) => {
+
+router.patch('/orders/:orderId', (req, res) => {
+  res.send('ok');
+});
+
+router.delete('/orders/:orderId', (req, res) => {
   res.send('ok');
 });
