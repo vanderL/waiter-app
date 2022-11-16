ef2bc118a4686dba476572379420eb591c7af833
Vander Lima <vnd.vander@gmail.com>
2022-11-16 00:30:22 -0300

    feat/list all routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,36 @@
+import { Router } from 'express';
+
+export const router = Router();
+
 // List categories
-
+router.get('/categories', (req, res) => {
+  res.send('ok');
+});
 // Create category
-
+router.post('/categories', (req, res) => {
+  res.send('ok');
+});
 // List products
-
+router.get('/products', (req, res) => {
+  res.send('ok');
+});
 // Create product
-
+router.post('/products', (req, res) => {
+  res.send('ok');
+});
 // Get products by category
-
+router.get('/categories/:id/products', (req, res) => {
+  res.send('ok');
+});
 // List orders
-
+router.get('/orders', (req, res) => {
+  res.send('ok');
+});
 // Create order
-
+router.post('/orders', (req, res) => {
+  res.send('ok');
+});
 // Change order status
+router.patch('/orders', (req, res) => {
+  res.send('ok');
+});
