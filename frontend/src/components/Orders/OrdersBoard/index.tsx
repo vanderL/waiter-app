bf0b7d676452ae9b1b91a0fbb39536484b60023f
Vander Lima <vnd.vander@gmail.com>
2022-11-19 03:16:40 -0300

    feat: interface Order

--- frontend/src/components/Orders/OrdersBoard/index.tsx
+++ frontend/src/components/Orders/OrdersBoard/index.tsx
@@ -1,42 +1,32 @@
+import { Order } from '../../../types/Order';
 import * as Sc from './styles';
 
-const order = [
-  {
-    mesa: 1,
-    quantity: 2,
-  },
-  {
-    mesa: 12,
-    quantity: 1,
-  },
-  {
-    mesa: 9,
-    quantity: 4,
-  },
-];
 
 interface OrdersBoardProps {
-  title: string,
-  icon: string,
+  title: string;
+  icon: string;
+  orders: Array<Order>;
 }
 
-export function OrdersBoard({ title, icon }: OrdersBoardProps) {
+export function OrdersBoard({ title, icon, orders }: OrdersBoardProps) {
   return (
     <Sc.Board>
       <header>
         <span>{icon}</span>
         <span>{title}</span>
-        <span>(1)</span>
+        <span>({orders.length})</span>
       </header>
 
-      <Sc.OrdersContainer>
-        {order.map(board => (
-          <button type="button" key={board.mesa}>
-            <strong>Mesa {board.mesa}</strong>
-            <span>{board.quantity} itens</span>
-          </button>
-        ))}
-      </Sc.OrdersContainer>
+      {orders.length > 0 ? (
+        <Sc.OrdersContainer>
+          {orders.map(board => (
+            <button type="button" key={board._id}>
+              <strong>Mesa {board.table}</strong>
+              <span>{board.products.length} itens</span>
+            </button>
+          ))}
+        </Sc.OrdersContainer>
+      ) : null}
     </Sc.Board>
   );
 }
--- frontend/src/components/Orders/index.tsx
+++ frontend/src/components/Orders/index.tsx
@@ -1,5 +1,84 @@
+import { Order } from '../../types/Order';
 import { OrdersBoard } from './OrdersBoard';
 import * as Sc from './styles';
+
+const orders: Order[] = [
+  {
+    '_id': '637870ecb0f82409ce0a95ba',
+    'table': '123',
+    'status': 'WAITING',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imagePath': 'b30a94e104e34874cc27-cerveja.png',
+          'price': 40,
+        },
+        'quantity': 3,
+        '_id': '637870ecb0f82409ce0a95bb'
+      },
+      {
+        'product': {
+          'name': 'Refri de bolo',
+          'imagePath': '01896470f5478a61abb2-8ba8cb4e54811f120b6207b8914472be.jpg',
+          'price': 40,
+        },
+        'quantity': 2,
+        '_id': '637870ecb0f82409ce0a95bc'
+      },
+      {
+        'product': {
+          'name': 'Refri de bolo',
+          'imagePath': '01896470f5478a61abb2-8ba8cb4e54811f120b6207b8914472be.jpg',
+          'price': 40,
+        },
+        'quantity': 2,
+        '_id': '637870ecb0f82409ce0a95bc'
+      }
+    ],
+  },
+  {
+    '_id': '637870ecb0f82409ce0a95ba',
+    'table': '33',
+    'status': 'IN_PRODUCTION',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imagePath': 'b30a94e104e34874cc27-cerveja.png',
+          'price': 40,
+        },
+        'quantity': 3,
+        '_id': '637870ecb0f82409ce0a95bb'
+      },
+    ],
+  },
+  {
+    '_id': '637870ecb0f82409ce0a95ba',
+    'table': '12',
+    'status': 'WAITING',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imagePath': 'b30a94e104e34874cc27-cerveja.png',
+          'price': 40,
+        },
+        'quantity': 3,
+        '_id': '637870ecb0f82409ce0a95bb'
+      },
+      {
+        'product': {
+          'name': 'Refri de bolo',
+          'imagePath': '01896470f5478a61abb2-8ba8cb4e54811f120b6207b8914472be.jpg',
+          'price': 40,
+        },
+        'quantity': 2,
+        '_id': '637870ecb0f82409ce0a95bc'
+      }
+    ],
+  }
+];
 
 export function Orders() {
   return (
@@ -7,14 +86,19 @@ export function Orders() {
       <OrdersBoard
         title={'Fila de espera'}
         icon={'🦽'}
+        orders={orders.filter(item => item.status === 'WAITING')}
       />
       <OrdersBoard
         title={'Em preparação'}
         icon={'🚀'}
+        orders={orders.filter(item => item.status === 'IN_PRODUCTION')}
+
       />
       <OrdersBoard
         title={'Pronto!'}
         icon={'🚕'}
+        orders={orders.filter(item => item.status === 'DONE')}
+
       />
     </Sc.Container>
   );
