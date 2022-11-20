2796b942c0019059630aa579abd11050bdec0c1a
Vander Lima <vnd.vander@gmail.com>
2022-11-20 01:58:14 -0300

    feat: List Itens in Modal

--- frontend/src/components/OrderModal/index.tsx
+++ frontend/src/components/OrderModal/index.tsx
@@ -1,6 +1,7 @@
 import * as Sc from './styles';
 import closeIcon from '../../assets/images/close-icon.svg';
 import { Order } from '../../types/Order';
+import { formatCurrency } from '../../utils/formatCurrency';
 
 interface OrderModalProps {
   visible: boolean;
@@ -41,11 +42,27 @@ export function OrderModal({ visible, order }: OrderModalProps) {
         <Sc.OrderDetails>
           <strong>Itens</strong>
 
-          {order.products.map(({ _id, product, quantity }) => (
-            <div className="item" key={_id}>
-              <img src={`http://localhost:3001/uploads/${product.imagePath}`} alt={product.name} />
-            </div>
-          ))}
+          <div className="order-items">
+            {order.products.map(({ _id, product, quantity }) => (
+              <div className="item" key={_id}>
+                <img
+                  src={`http://localhost:3001/uploads/${product.imagePath}`}
+                  alt={product.name}
+                  height="80"
+                  width="80"
+                />
+                <span className="quantity">x{quantity}</span>
+                <div className="product-details">
+                  <strong>{product.name}</strong>
+                  <span>
+                    {
+                      formatCurrency(product.price)
+                    }
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
         </Sc.OrderDetails>
       </Sc.ModalBody>
     </Sc.Overlay>
