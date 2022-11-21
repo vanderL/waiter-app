decac2c0574ba6a48c666e64f5719d54e13453a7
Vander Lima <vnd.vander@gmail.com>
2022-11-20 22:50:23 -0300

    feat: configs inital

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,23 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Text } from './src/components/Text';
 
 export default function App() {
+  const [isFontLoader] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-500': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!isFontLoader) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open!</Text>
+    <View >
+      <Text weight='600' size={24}>Open!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
